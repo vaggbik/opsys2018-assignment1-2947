c3f5fade8ae525f7abad00fe7fa8f6e258c8b81c
Ευάγγελος Μπίκας <32570379+vaggbik@users.noreply.github.com>
2018-12-09 06:05:55 +0200

    Update script1b.sh

--- script1b.sh
+++ script1b.sh
@@ -1,1 +1,67 @@
-.
+#!/bin/bash
+
+if [[ ! -d ./sites/ ]]
+then
+        mkdir sites
+fi
+
+file="../File.txt"
+
+cd sites
+
+echo > teliko
+
+if [ -f $file ]
+then
+        while IFS='' read -r site || [[ -n "$site" ]]
+                do
+                        if [[ $site != "#"* ]];
+                                then
+                                filesite="$(echo "${site#*//}" | tr -d /)";
+                                if [[ ! -f $filesite ]]
+                                then
+                                        touch $filesite
+                                        echo "$site INIT" >> teliko
+                                        wget "$site" -q -O $filesite;
+                                        if [[ ! -s $filesite ]];
+                                        then
+                                                >&2 echo "$site FAILED" >> teliko ;
+                                        fi
+                                else
+                                        wget "$site" -q -O temp.txt
+                                        if [[ ! -s temp.txt ]]
+                                        then
+                                                >&2 echo "$site FAILED" >> teliko ;
+                                        else
+                                                if ! diff $filesite temp.txt >/dev/null ;
+                                                then
+                                                        echo "$site" >> teliko
+                                                        wget "$site" -q -O $filesite;
+
+                                                fi
+                                        fi
+                                fi
+                        fi
+                done <"$file"
+                
+else
+        echo "Error 404: File.txt with websites not found"
+        echo "Do you want to create File.txt? (y/n)"    
+                read a;
+                if [[ $a == "y" ]]
+                then
+                        touch ../File.txt;
+                fi
+        while [ $a != "y" ] && [ $a != "n" ]
+        do
+                echo "you must answer with y for yes or n for no."
+                read a;
+                if [[ $a == "y" ]]
+                then
+                        touch ../File.txt;
+                fi
+        done;
+fi
+
+cat ./teliko
+rm teliko
